95b3274a1db07c3366cc5cd16b23f1d7e06bafb4
WGS <53532339+GuangShuaiWang@users.noreply.github.com>
2025-01-12 19:37:15 +0800

    Update build_dockerfile.sh

--- build_dockerfile.sh
+++ build_dockerfile.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-$DOCKERHUB_USERNAME="wanggsh"
-DOCKERFILE_PATH="colabfold_dockerfile"
-IMAGE_NAME="colabfold_gpu" 
-IMAGE_TAG="latest"           
+# 定义变量
+DOCKERHUB_USERNAME="wanggsh"  # Docker Hub 用户名
+DOCKERFILE_PATH="colabfold_dockerfile"  # Dockerfile 路径
+IMAGE_NAME="colabfold_gpu"  # 镜像名
+IMAGE_TAG="latest"  # 标签
 
+# 构建镜像
 echo "Building Docker image..."
-docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME -f $DOCKERFILE_PATH .
+docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG -f $DOCKERFILE_PATH .
 
+# 推送镜像到 Docker Hub
 echo "Pushing Docker image to Docker Hub..."
 docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
 
+# 结束提示
 echo "Build and push completed!"
