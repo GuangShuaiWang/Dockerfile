bfb790d710727307a7bcf40a8c8b05964b1c986f
WGS <53532339+GuangShuaiWang@users.noreply.github.com>
2025-01-12 19:33:52 +0800

    Update build_dockerfile.sh

--- build_dockerfile.sh
+++ build_dockerfile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+$DOCKERHUB_USERNAME="wanggsh"
 DOCKERFILE_PATH="colabfold_dockerfile"
 IMAGE_NAME="colabfold_gpu" 
 IMAGE_TAG="latest"           
 
 echo "Building Docker image..."
-docker build -f $DOCKERFILE_PATH -t $DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG .
+docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME -f $DOCKERFILE_PATH .
 
 echo "Pushing Docker image to Docker Hub..."
 docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG
